058d279850ee89889fbc0c3a553c77a5e7f01755
AvrahamFerdman <80028303+AvrahamFerdman@users.noreply.github.com>
2023-09-10 20:16:03 +0300

    Adding coupons generation algorithms

---
+++ constants.py
@@ -0,0 +1,9 @@
+from enum import Enum, auto
+
+class CouponsKind(Enum):
+    SHUFERSAL = auto()
+    WOLT = auto()
+
+SHUFERSAL_COUPONS = [30, 40, 50, 100, 200]
+
+WOLT_COUPONS = [30, 40, 50, 100, 200]
